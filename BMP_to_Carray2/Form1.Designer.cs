e32781ce5e743ad6e32b9ae42b6404f411e46da1
Louis Sather <louis@springbokinstruments.com>
2023-09-15 10:13:00 -0500

    Add project files.

---
+++ BMP_to_Carray2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BMP_to_Carray2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 430);
+            this.button1.MinimumSize = new System.Drawing.Size(0, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(687, 40);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.button1_DragDrop);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.button1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.MinimumSize = new System.Drawing.Size(0, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(687, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy to clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(687, 390);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 470);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "BMP to Const Byte Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BMP_to_Carray2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace BMP_to_Carray2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Dock = DockStyle.Fill;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private string GenerateConstByteArrayDeclaration(byte[] array, string variableName, Bitmap bmp)
+        {
+            var builder = new StringBuilder();
+
+            // Add dimensions comment
+            builder.AppendLine($"// Dimensions: {bmp.Width}x{bmp.Height}");
+
+            builder.AppendLine($"const uint8_t {variableName}[] =");
+            builder.AppendLine("{");
+
+            int bytesPerRow = (bmp.Width + 7) / 8; // Round up to the nearest byte
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                builder.Append($"0x{array[i]:X2}, ");
+                if ((i + 1) % bytesPerRow == 0) builder.AppendLine();
+            }
+
+            builder.AppendLine("};");
+            return builder.ToString();
+        }
+
+        private byte[] ConvertBitmapToByteArray(Bitmap bmp)
+        {
+            int bytesPerRow = (bmp.Width + 7) / 8; // Round up to the nearest byte
+            byte[] formattedData = new byte[bytesPerRow * bmp.Height];
+
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+
+            // Stride is the width of a single row of the image in bytes, taking into account any padding added to align rows to 4 bytes
+            int stride = bmpData.Stride;
+
+            unsafe
+            {
+                byte* ptr = (byte*)bmpData.Scan0;
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bytesPerRow; x++)
+                    {
+                        formattedData[y * bytesPerRow + x] = ptr[y * stride + x];
+                    }
+                }
+            }
+
+            bmp.UnlockBits(bmpData);
+            return formattedData;
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                string bmpPath = files[0];
+                var bmp = new Bitmap(bmpPath);
+                var bmp1bpp = bmp.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height), PixelFormat.Format1bppIndexed);
+                var byteArray = ConvertBitmapToByteArray(bmp1bpp);
+                var arrayName = Path.GetFileNameWithoutExtension(bmpPath);
+                arrayName = arrayName.Replace(" ", "_");
+                var byteArrayString = GenerateConstByteArrayDeclaration(byteArray, arrayName, bmp);
+                textBox1.Text = GenerateConstByteArrayDeclaration(byteArray, arrayName, bmp1bpp);
+                textBox1.Visible = true;
+                button1.Dock = DockStyle.Bottom;
+                button1.Text = "Drag New Image Here";
+                button1.BackgroundImage = null;
+                button2.Visible = true;
+                textBox1.Dock = DockStyle.Fill;
+
+
+            }
+        }
+
+        private void button1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(textBox1.Text);
+        }
+    }
+}
